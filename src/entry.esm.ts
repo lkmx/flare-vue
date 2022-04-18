c07e7279cfd7c6d2c876d788ee218d245dde7728
Victor Valle <victor.valle@lkmx.io>
2022-04-18 03:29:46 -0700

    Added flare components

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,7 +1,7 @@
 import { App, Plugin } from 'vue';
 
 // Import vue components
-import * as components from '@/lib-components/index';
+import * as components from '@/components/index';
 
 // install function executed by Vue.use()
 const install: Exclude<Plugin['install'], undefined> = function installFlareVue(app: App) {
@@ -15,4 +15,4 @@ export default install;
 
 // To allow individual component use, export components
 // each can be registered via Vue.component()
-export * from '@/lib-components/index';
+export * from '@/components/index';
