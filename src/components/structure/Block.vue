d6b225c82f18e855f726d542cb79b77263094c2c
Victor Valle <victor.valle@lkmx.io>
2022-04-18 03:30:02 -0700

    Added flare components

---
+++ src/components/structure/Block.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="--flare --flare-block">
+    <div class="content"><div class="box"><slot></slot></div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+};
+</script>
---
+++ src/components/ui/SimpleFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="--flare-simple-footer">
+    <columns :mode="mode">
+      <block>
+        <slot></slot>
+      </block>
+    </columns>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
