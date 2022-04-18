b1d9f3d9140afbabefeb0c5ee4b5b113893136f1
Victor Valle <victor.valle@lkmx.io>
2022-04-18 01:40:17 -0700

    sfc init template plus flare import

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { FlareVueSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  FlareVueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <flare-vue-sample />
+  </div>
+</template>
