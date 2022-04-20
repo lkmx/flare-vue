b40cc535eb7751e646429419f84a3217906915c7
Victor Valle <victor.valle@lkmx.io>
2022-04-20 02:07:29 -0700

    Fixed SCSS import

---
+++ module.d.ts
@@ -0,0 +1,13 @@
+declare module '@lkmx/flare-vue';
+
+/* eslint-disable import/prefer-default-export */
+export { default as Page } from './src/structure/Page.vue';
+export { default as Column } from './src/structure/Columns.vue';
+export { default as Columns } from './src/structure/Columns.vue';
+export { default as Block } from './src/structure/Block.vue';
+
+export { default as SideBarLayout } from './src/ui/layouts/SideBarLayout.vue';
+export { default as SimpleLayout } from './src/ui/layouts/SimpleLayout.vue';
+
+export { default as SimpleHeader } from './src/ui/basic/SimpleHeader.vue';
+export { default as SimpleFooter } from './src/ui/basic/SimpleFooter.vue';
