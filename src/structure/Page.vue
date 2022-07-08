7e7c6d4b5af41d4c1cb8ca39653cfb4c7f332e39
Asael <asael.medina@lkmx.io>
2022-07-08 16:34:46 -0700

    Add and remove flare class to body

--- src/structure/Page.vue
+++ src/structure/Page.vue
@@ -3,3 +3,13 @@
     <slot></slot>
   </main>
 </template>
+<script>
+export default {
+  beforeCreate() {
+    document.body.classList.add('--flare-hidden')
+  },
+  mounted() {
+    document.body.classList.remove('--flare-hidden')
+  }
+}
+</script>
